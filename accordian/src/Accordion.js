e6e96955b0a2bc977f68b920f4a39b344289db79
chitha_srinath <chithasaireddy@gmail.com>
2022-01-12 12:08:20 +0530

    onecomponentALLdirections

--- accordian/src/Accordion.js
+++ accordian/src/Accordion.js
@@ -3,15 +3,37 @@ import "./Accordion.css"
 import { useState } from 'react'
 
 export default function Accordion() {
-    const [show, setshow] = useState(false)
+    const [showDown, setshowDown] = useState(false)
+    const [showUp, setshowUp] = useState(false)
+    const [showLeft, setshowLeft] = useState(false)
+    const [showRight, setshowRight] = useState(false)
     return (
         <>
+            {showUp ?
+                <div className="section">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam harum et dignissimos a voluptatibus quos quia ipsam. Amet facere doloribus in temporibus, totam commodi animi quibusdam. Illum, repellendus enim!</p>
+                </div> : null
+            }
+
             <div className='header'>
-                <h3>Accordion - 1</h3>
-                <button onClick={()=>setshow(true?show ===false:false)}>click to {show?"close":"open"}</button>
+                {showLeft ?
+                    <div className="sectionLeft">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam harum et dignissimos a voluptatibus quos quia ipsam. Amet facere doloribus in temporibus, totam commodi animi quibusdam. Illum, repellendus enim!</p>
+                    </div> : null
+                }
+                <div><button onClick={() => setshowLeft(true ? showLeft === false : false)}>click to {showLeft ? "close left" : "open left"}</button></div>
+                <div><button onClick={() => setshowRight(true ? showRight === false : false)}>click to {showRight ? "close right" : "open right"}</button></div>
+                <div><button onClick={() => setshowDown(true ? showDown === false : false)}>click to {showDown ? "close down" : "open down"}</button></div>
+                <div><button onClick={() => setshowUp(true ? showUp === false : false)}>click to {showUp ? "close up" : "open up"}</button></div>
+
+                {showRight ?
+                    <div className="sectionRight">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam harum et dignissimos a voluptatibus quos quia ipsam. Amet facere doloribus in temporibus, totam commodi animi quibusdam. Illum, repellendus enim!</p>
+                    </div> : null
+                }
             </div>
-            {show ?
-                <div className="content">
+            {showDown ?
+                <div className="section">
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio numquam harum et dignissimos a voluptatibus quos quia ipsam. Amet facere doloribus in temporibus, totam commodi animi quibusdam. Illum, repellendus enim!</p>
                 </div> : null
             }
